99a7e50347b07026d9122a059e5af89696442a58
AnneVictoria <avduclaux@gmail.com>
2020-12-14 16:11:44 +0100

    add sign up

--- eatlikealocal/app.js
+++ eatlikealocal/app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+require("./config/mongo");
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -9,16 +10,16 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const path = require("path");
 
-mongoose
-  .connect("mongodb://localhost/eatlikealocal", { useNewUrlParser: true })
-  .then((x) => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo", err);
-  });
+// mongoose
+//   .connect("mongodb://localhost/eatlikealocal", { useNewUrlParser: true })
+//   .then((x) => {
+//     console.log(
+//       `Connected to Mongo! Database name: "${x.connections[0].name}"`
+//     );
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo", err);
+//   });
 
 const app_name = require("./package.json").name;
 const debug = require("debug")(
--- eatlikealocal/routes/index.js
+++ eatlikealocal/routes/index.js
@@ -30,4 +30,28 @@ router.get("/restaurant/:id", async (req, res, next) => {
   }
 });
 
+//  TO REVIEW =>  GET Sign-up page
+const UserModel = require("./../model/User");
+const CommentModel = require("./../model/Comment");
+
+router.get("/signup", async (req, res, next) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    next(err);
+  }
+});
+
+// TO REVIEW =>  POST Sign-up page
+router.post("/signup", async (req, res, next) => {
+  const newUser = { ...req.body };
+
+  try {
+    await UserModel.create(newUser);
+    res.redirect("/signin/home"); //redirect to sign-in home?
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
