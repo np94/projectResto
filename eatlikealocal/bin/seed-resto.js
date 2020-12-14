d0f59fa109ec05bc2fce3000641d40d283c58f2a
Intan Paoli <intankhairunnisa@gmail.com>
2020-12-14 12:54:54 +0100

    updated seed-resto.js

--- eatlikealocal/bin/seed-resto.js
+++ eatlikealocal/bin/seed-resto.js
@@ -10,12 +10,105 @@ const restaurants = [
     address: {
       number: "6R",
       streetName: "Via dei palchetti",
-      city: "Firenze",
+      city: "Florence",
       zipCode: "50100",
       country: "Italy",
     },
     description: "Nice italian food",
     price: "€€",
+    // user: "["default"]",
+    // comment: default,
+    picture:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+  {
+    name: "L'Osteria di Giovanni",
+    genre: "Italian",
+    website: "www.osteriadigiovanni.it",
+    address: {
+      number: "22",
+      streetName: "Via del Moro",
+      city: "Florence",
+      zipCode: "50100",
+      country: "Italy",
+    },
+    description: "Typical tuscani cuisine",
+    price: "€€",
+    // user: "["default"]",
+    // comment: default,
+    picture:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+
+  {
+    name: "Trattoria Mario",
+    genre: "Italian",
+    website: "www.trattoriamario.com",
+    address: {
+      number: "2",
+      streetName: "Via Rosina",
+      city: "Florence",
+      zipCode: "50100",
+      country: "Italy",
+    },
+    description: "This restaurant exist since 1953",
+    price: "€",
+    // user: "["default"]",
+    // comment: default,
+    picture:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+  {
+    name: "21 MAGYAR VENDÉGLÖ",
+    genre: "Hungarian",
+    website: "www.21restaurant.hu",
+    address: {
+      number: "21",
+      streetName: "Fortuna utca",
+      city: "Budapest",
+      zipCode: "1014",
+      country: "Hungary",
+    },
+    description: "Traditional Hungarian Food",
+    price: "€",
+    // user: "["default"]",
+    // comment: default,
+    picture:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+  {
+    name: "FATÁL",
+    genre: "Hungarian",
+    website: "www.fatalrestaurant.com",
+    address: {
+      number: "67",
+      streetName: "V Váci utca",
+      city: "Budapest",
+      zipCode: "1056",
+      country: "Hungary",
+    },
+    description:
+      "The restaurant serves massive hungarian meal, it could be fatal!",
+    price: "€€",
+    // user: "["default"]",
+    // comment: default,
+    picture:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+  {
+    name: "BAGOLIVÁR",
+    genre: "Hungarian",
+    website: "www.bagolyvar.com",
+    address: {
+      number: "4",
+      streetName: " Gundel Károly út ",
+      city: "Budapest",
+      zipCode: "1146",
+      country: "Hungary",
+    },
+    description:
+      "Family restaurant for local pork & beef dishes, in a century-old wooden house with dark beams.",
+    price: "€",
     // user: "["default"]",
     // comment: default,
     picture:
