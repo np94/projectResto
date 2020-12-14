1a581d9a82324fe3edcf2b8e1ea60acaf66cf486
AnneVictoria <avduclaux@gmail.com>
2020-12-14 12:07:59 +0100

    seed user added

--- eatlikealocal/bin/seed-user.js
+++ eatlikealocal/bin/seed-user.js
@@ -1,34 +1,29 @@
-// require("dotenv").config();
-// require("../config/mongo"); // fetch the db connection
-// const RestaurantModel = require("../model/Restaurant"); // fetch the model to validate our user document before insertion (in database)
+require("dotenv").config();
+require("../config/mongo"); // fetch the db connection
+const UserModel = require("../model/User"); // fetch the model to validate our user document before insertion (in database)
 
-// const restaurants = [
-//     {
-//         firstname: "Anne",
-//         lastname: "Duclaux",
-//         email: "avduclaux@ironhack.fr",
-//         password: "000",
-//         city: "Paris",
-//         restaurant: [{ type: Schema.Types.ObjectId, ref: "restaurant" }],
-//         comment: [{ type: Schema.Types.ObjectId, ref: "comment" }],
-//         avatar: {
-//           type: String,
-//           default:
-//             "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
-//         },
+const users = [
+  {
+    firstname: "Anne",
+    lastname: "Duclaux",
+    email: "annevic@ironhack.fr",
+    password: "911",
+    city: "Paris",
+    // restaurant: [{ type: Schema.Types.ObjectId, ref: "restaurant" }],
+    // comment: [{ type: Schema.Types.ObjectId, ref: "comment" }],
+    avatar:
+      "https://www.pinclipart.com/picdir/middle/157-1578186_user-profile-default-image-png-clipart.png",
+  },
+];
 
-// ];
+async function insertUsers() {
+  try {
+    await UserModel.deleteMany();
+    const inserted = await UserModel.insertMany(users);
+    console.log(`seed users done : ${inserted.length} documents inserted !`);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
-// async function insertRestaurants() {
-//   try {
-//     await RestaurantModel.deleteMany();
-//     const inserted = await RestaurantModel.insertMany(restaurants);
-//     console.log(
-//       `seed restaurants done : ${inserted.length} documents inserted !`
-//     );
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-
-// insertRestaurants();
+insertUsers();
