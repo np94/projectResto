44e8fa7a32de8a84d1bb935585ac29d0b0f6920c
Intan Paoli <intankhairunnisa@gmail.com>
2020-12-14 14:25:38 +0100

    route allresto and oneresto

--- eatlikealocal/routes/index.js
+++ eatlikealocal/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const RestaurantModel = require("./../model/Restaurant");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
