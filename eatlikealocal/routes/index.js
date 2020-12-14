815af54f10030ce77d4d95204462cd06a3bd2af5
AnneVictoria <avduclaux@gmail.com>
2020-12-14 14:05:20 +0100

    updated views

--- eatlikealocal/routes/index.js
+++ eatlikealocal/routes/index.js
@@ -11,8 +11,20 @@ router.get("/restaurant", async (req, res, next) => {
   try {
     const allRestaurants = await RestaurantModel.find();
     res.render("all-restaurants", { allRestaurants });
+    console.log(allRestaurants);
   } catch (err) {
     console.log(err);
+    next(err);
+  }
+});
+
+// Display one restaurant
+router.get("/restaurant/:id", async (req, res, next) => {
+  try {
+    const oneRestaurant = await RestaurantModel.findById(req.params.id);
+    res.render("one-restaurant", oneRestaurant);
+    console.log(oneRestaurant);
+  } catch (err) {
     next(err);
   }
 });
