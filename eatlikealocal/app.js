f8c7c7584f6d8c01b882a48f3109c22ed7fd4743
AnneVictoria <avduclaux@gmail.com>
2020-12-16 16:21:56 +0100

    comment

--- eatlikealocal/app.js
+++ eatlikealocal/app.js
@@ -42,7 +42,7 @@ app.use(cookieParser());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    cookie: { maxAge: 60000 }, // in millisec
+    cookie: { maxAge: 6000000 }, // in millisec
     store: new MongoStore({
       mongooseConnection: mongoose.connection, // you can store session infos in mongodb :)
       ttl: 24 * 60 * 60, // 1 day
