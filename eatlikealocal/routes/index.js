5470a7946fe60e47cefdf8abbc3f2f5728239814
Ann Lampa <riann08@yahoo.com>
2020-12-16 17:24:15 +0100

    filter restaurants by city ok

--- eatlikealocal/routes/index.js
+++ eatlikealocal/routes/index.js
@@ -6,8 +6,10 @@ const RestaurantModel = require("./../model/Restaurant");
 router.get("/", async (req, res, next) => {
   try {
   const allRestaurants = await RestaurantModel.find();
-  console.log(allRestaurants);
-  res.render("index", {allRestaurants} );
+  const cities = [...new Set(allRestaurants.map(restau => restau.city ))];
+  console.log(cities);
+console.log("------------------------------------");
+  res.render("index", {allRestaurants,cities} );
 } catch(err) {
   console.log(err);
   next(err);
@@ -19,21 +21,34 @@ router.get("/restaurant", async (req, res, next) => {
   try {
     const allRestaurants = await RestaurantModel.find().populate("user");
     res.render("all-restaurants", allRestaurants);
-    console.log(allRestaurants);
+    //console.log(allRestaurants);
   } catch (err) {
     console.log(err);
     next(err);
   }
 });
 
-// Display one restaurant
+//Display all restaurants by city
+router.post("/restaurant/city", async (req, res, next) => {
+ 
+  try {
+    const allRestaurantsByCity = await RestaurantModel.find({city: req.body.city});
+
+    console.log("---------------all resto", allRestaurantsByCity);
+ res.render("all-restaurants-by-city", {allRestaurantsByCity});
+ } catch(err) {
+   console.log(err);
+   next(err);
+ }
+});
+
 router.get("/restaurant/:id", async (req, res, next) => {
   try {
     const oneRestaurant = await RestaurantModel.findById(
       req.params.id
     ).populate("user");
     res.render("one-restaurant", oneRestaurant);
-    console.log(oneRestaurant);
+   // console.log(oneRestaurant);
   } catch (err) {
     next(err);
   }
