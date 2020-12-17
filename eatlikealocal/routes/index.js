8a0c08e47a50ae5516fcfb3b1b68f7c4b76fd5be
Ann Lampa <riann08@yahoo.com>
2020-12-17 10:32:50 +0100

    removed console log lines

--- eatlikealocal/routes/index.js
+++ eatlikealocal/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const RestaurantModel = require("./../model/Restaurant");
+const UserModel = require("./../model/User");
+const CommentModel = require("./../model/Comment");
 
-/* GET home page */
+// Display home page
 router.get("/", async (req, res, next) => {
   try {
     const allRestaurants = await RestaurantModel.find();
@@ -41,20 +43,16 @@ router.post("/restaurant/city", async (req, res, next) => {
   }
 });
 
+//Display one restaurant
 router.get("/restaurant/:id", async (req, res, next) => {
   try {
     const oneRestaurant = await RestaurantModel.findById(
       req.params.id
     ).populate("user");
     res.render("one-restaurant", oneRestaurant);
-    // console.log(oneRestaurant);
   } catch (err) {
     next(err);
   }
 });
-
-//  TO REVIEW =>  GET Sign-up page
-const UserModel = require("./../model/User");
-const CommentModel = require("./../model/Comment");
 
 module.exports = router;
